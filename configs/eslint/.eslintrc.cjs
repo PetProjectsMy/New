efc133003bbca2dfbbe7eabaaab1553e9c22cca2
Artur Isaev <isaev.artur.1999@mail.ru>
2023-07-02 22:40:20 +0600

    configs(eslint): add pattern for not ignoring files with names starting with dot

--- configs/eslint/.eslintrc.cjs
+++ configs/eslint/.eslintrc.cjs
@@ -4,7 +4,12 @@ const { pluginsConfigOverride } = require('./pluginsConfigs/_index.cjs');
 const { eslintRules } = require('./rules/_index.cjs');
 
 const eslintConfig = {
-  ignorePatterns: ['dist/', 'build/', 'node_modules/'],
+  ignorePatterns: [
+    'dist/',
+    'build/',
+    'node_modules/',
+    '!.*', // not ignore files started with dot
+  ],
   overrides: [...languagesConfigOverride, ...pluginsConfigOverride, ...perFilesConfigsOverride],
   root: true,
   rules: eslintRules,
