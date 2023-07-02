05fea9b751abdf84fb515ee559e2eaf9d798e358
Artur Isaev <isaev.artur.1999@mail.ru>
2023-07-02 23:54:02 +0600

    configs(stylelint): turn off 'custom-property-empty-line-before' rule

--- configs/stylelint/.stylelintrc.cjs
+++ configs/stylelint/.stylelintrc.cjs
@@ -1,5 +1,8 @@
 const stylelintConfig = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
+  rules: {
+    'custom-property-empty-line-before': null,
+  },
 };
 
 module.exports = stylelintConfig;
